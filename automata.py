d5f1feb98699f0a2969c7b907b6a38fcb74eb032
Ivan Viveros <ivan.viveros@correounivalle.edu.co>
2019-09-30 23:16:23 -0500

    actualizacion algoritmo

--- automata.py
+++ automata.py
@@ -2,6 +2,8 @@ import time
 
 
 def recorrer(automata):
+    estados_aceptacion_recorridos_sin_ciclos = set()
+    estados_aceptacion_recorridos_con_ciclos = set()
     def buscar_ciclos(automata, estados_recorridos=None, estado_actual=0):
         estados_recorridos_iteracion = (
             estados_recorridos.copy() if estados_recorridos else []
@@ -9,74 +11,86 @@ def recorrer(automata):
         for estado_siguiente in range(len(automata["matriz_transicion"])):
             if automata["matriz_transicion"][estado_actual][estado_siguiente] != None:
                 if estado_siguiente in estados_recorridos_iteracion:
-                    return False
+                    if estado_siguiente in automata["estados_aceptacion"]:
+                        estados_aceptacion_recorridos_sin_ciclos.remove(estado_siguiente)
+                        estados_aceptacion_recorridos_con_ciclos.add(estado_siguiente)
+                    return True
 
                 estados_recorridos_iteracion.append(estado_siguiente)
-                if not buscar_ciclos(
+
+                if estado_siguiente in automata["estados_aceptacion"]:
+                    estados_aceptacion_recorridos_sin_ciclos.add(estado_siguiente)
+
+                encontro_ciclos = buscar_ciclos(
                     automata, estados_recorridos_iteracion, estado_siguiente
-                ):
-                    return False
+                )
+                if encontro_ciclos:
+                    return True
 
-        if (
-            automata["matriz_transicion"][estado_actual]
-            == [None] * len(automata["matriz_transicion"])
-            and estado_actual not in automata["estados_aceptacion"]
-        ):
-            return False
+        if estado_actual in automata["estados_aceptacion"]:
+            estados_aceptacion_recorridos_sin_ciclos.add(estado_actual)
 
-        return True
+        return False
 
-    reconoce_lenguaje_finito = buscar_ciclos(automata)
-    if reconoce_lenguaje_finito:
+    encontro_ciclos = buscar_ciclos(automata)
+
+    reconoce_lenguaje_finito = False
+
+    if encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) == 0 and len(estados_aceptacion_recorridos_con_ciclos) == 0:
+        reconoce_lenguaje_finito = False
+    elif encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) > 0 and len(estados_aceptacion_recorridos_con_ciclos) == 0:
+        reconoce_lenguaje_finito = True
+    elif not encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) == 0 and len(estados_aceptacion_recorridos_con_ciclos) == 0:
+        reconoce_lenguaje_finito = False
+    elif not encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) > 0 and len(estados_aceptacion_recorridos_con_ciclos) == 0:
+        reconoce_lenguaje_finito = True
+    elif encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) == 0 and len(estados_aceptacion_recorridos_con_ciclos) > 0:
+        reconoce_lenguaje_finito = False
+    elif encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) > 0 and len(estados_aceptacion_recorridos_con_ciclos) > 0:
+        reconoce_lenguaje_finito = False
+    elif not encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) == 0 and len(estados_aceptacion_recorridos_con_ciclos) > 0:
+        reconoce_lenguaje_finito = False
+    elif not encontro_ciclos and len(estados_aceptacion_recorridos_sin_ciclos) > 0 and len(estados_aceptacion_recorridos_con_ciclos) > 0:
+        reconoce_lenguaje_finito = False
+
+    if reconoce_lenguaje_finito is True:
         print("El automata reconoce un lenguaje finito")
-    else:
-        print("El automata reconoce un lenguaje infinito")
-
+    elif reconoce_lenguaje_finito is False:
+        print("El automata no reconoce un lenguaje finito")
 
 automata_lenguaje_finito = {
     "alfabeto": ["a", "b"],
     "matriz_transicion": [
-        [None, "a", None],
-        [None, None, ["a", "b"]],
-        [None, None, None],
+        [None, "a", None, "b"],
+        [None, None, "a", "b"],
+        [None, None, None, ["a", "b"]],
+        [None, None, None, ["a", "b"]],
     ],
     "estados_aceptacion": [2],
-}
-
-
-automata_lenguaje_finito2 = {
-    "alfabeto": ["a", "b"],
-    "matriz_transicion": [
-        [None, "a", None],
-        [None, None, None],
-        [None, None, None],
-    ],
-    "estados_aceptacion": [1],
 }
 
 automata_lenguaje_infinito = {
     "alfabeto": ["a", "b"],
     "matriz_transicion": [
-        [None, "a", None, None],
+        [None, "a", None, "b"],
         [None, None, "a", "b"],
-        [None, None, None, "b"],
-        [None, None, None, "b"],
+        [None, None, None, ["a", "b"]],
+        [None, None, None, ["a", "b"]],
     ],
-    "estados_aceptacion": [2, 3],
+    "estados_aceptacion": [3],
 }
 
 automata_lenguaje_infinito2 = {
     "alfabeto": ["a", "b"],
     "matriz_transicion": [
-        [None, "a", None, None],
+        [None, "a", None, "b"],
         [None, None, "a", "b"],
         [None, None, None, ["a", "b"]],
-        [None, None, "a", None],
+        [None, None, None, ["a", "b"]],
     ],
-    "estados_aceptacion": [1, 2, 3],
+    "estados_aceptacion": [2, 3],
 }
 
 recorrer(automata_lenguaje_finito)
-recorrer(automata_lenguaje_finito2)
 recorrer(automata_lenguaje_infinito)
 recorrer(automata_lenguaje_infinito2)
